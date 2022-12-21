62fb493ef17d3e11946eb37d6e8f9bdb2ac0a17e
MrWanted <wanted.lepota@gmail.com>
2022-12-21 22:27:00 +0200

    add withdrawal service

--- src/main/java/App/service/WithdrawalService.java
+++ src/main/java/App/service/WithdrawalService.java
@@ -2,15 +2,16 @@ package App.service;
 
 import App.entity.Withdrawal;
 import App.vo.WithdrawVO;
-import org.springframework.http.ResponseEntity;
+
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
 
 public interface WithdrawalService {
     Withdrawal save(Withdrawal withdrawal);
+
+    Withdrawal updateWithdrawal(Withdrawal withdrawal, Integer withdrawalId);
     List<Withdrawal> findAll();
     Withdrawal submitWithdrawal(Integer investorId, Integer productId, BigDecimal amount);
-    Optional<Withdrawal> findById(Integer id);
+    Withdrawal findById(Integer id);
     Withdrawal update(WithdrawVO withdrawal);
 }
