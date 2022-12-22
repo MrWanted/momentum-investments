9651ca5ed23229cba698839360e781e5ae95c4f7
MrWanted <wanted.lepota@gmail.com>
2022-12-22 07:33:41 +0200

    remove schema definitions

--- src/main/java/App/entity/Person.java
+++ src/main/java/App/entity/Person.java
@@ -10,42 +10,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Setter
-@Getter
-@ToString
+@Data
 public class Person implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "person_id", nullable = false)
-    @Schema(description = "The generated ID when saved in database",
-            name = "id"
-    )
     private Integer id;
-    @Schema(description = "first name of the investor",
-            name = "first name",
-            required = true,
-            example = "Kamino")
-    @Size(min = 3, max = 40)
-    @Column(name = "name", nullable = false)
-    private String firstName;
+
+    private String name;
 
     private String surname;
-    @Schema(description = "gender",
-            name = "gender",
-            required = true,
-            example = "Male, Female, X")
-    @Size(min = 1, max = 6)
     private String gender;
-    @Column(name = "dateofbirth", nullable = false)
     private String dateOfBirth;
     private int age;
-    @Schema(description = "identity number of the investor",
-            name = "identity number",
-            required = true)
-    @Column(name = "identitynumber", nullable = false)
-    private String identityNumber;
+
+    private String idno;
+
     @OneToMany(mappedBy = "person",cascade = CascadeType.ALL)
     private List<Contact> contact = new ArrayList<>(1);
 
