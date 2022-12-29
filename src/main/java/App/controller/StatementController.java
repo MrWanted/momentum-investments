3c6c1fcb12d77d2ce062f888c672094970effbca
MrWanted <wanted.lepota@gmail.com>
2022-12-29 07:41:43 +0200

    add endpoint to get statement by investor and product id's

--- src/main/java/App/controller/StatementController.java
+++ src/main/java/App/controller/StatementController.java
@@ -1,9 +1,7 @@
 package App.controller;
 
-import App.entity.Person;
-import App.entity.Product;
-import App.entity.Withdrawal;
-import App.service.WithdrawalService;
+import App.entity.Statement;
+import App.service.StatementService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -17,34 +15,38 @@ import java.math.BigDecimal;
 @RequestMapping("/rest/api/withdrawal")
 @Slf4j
 @Data
-public class WithdrawalController {
-    private final WithdrawalService service;
+public class StatementController {
+    private final StatementService service;
 
     @Operation(summary = "Withdrawals", description = "save and update audit trail for withdrawal")
     @PostMapping("")
-    public ResponseEntity save(@RequestBody Withdrawal withdrawal) {
+    public ResponseEntity save(@RequestBody Statement withdrawal) {
         log.info("Saving a withdrawal...");
         return new ResponseEntity<>(service.save(withdrawal), HttpStatus.OK);
     }
     @Operation(summary = "Withdrawals", description = "get all Investors withdrawals")
     @GetMapping("")
-    public ResponseEntity<Iterable<Withdrawal>> getAllInvestorsWithdrawals(){
-        Iterable<Withdrawal> list = service.findAll();
+    public ResponseEntity<Iterable<Statement>> getAllInvestorsWithdrawals(){
+        Iterable<Statement> list = service.findAll();
         log.info("getting all withdrawals");
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
     @Operation(summary = "Submit Withdraw", description = "initiate withdrawal process with the given information of the investor.")
     // document validation requirements
-    @PostMapping("/{investorId}/product/{productID}/amount/{amount}")
-    public ResponseEntity submitWithdrawal(@PathVariable("investorId") Integer investorId, @PathVariable("productID") Integer productID,
+    @PostMapping("investor/{investorId}/product/{productId}/amount/{amount}")
+    public ResponseEntity submitWithdrawal(@PathVariable("investorId") Integer investorId, @PathVariable("productId") Integer productId,
                            @PathVariable("amount") BigDecimal withdrawalAmount){
         log.info("submitting investment withdrawal...");
-        Withdrawal withdrawal = service.submitWithdrawal(investorId, productID, withdrawalAmount);
+        Statement withdrawal = service.submitWithdrawal(investorId, productId, withdrawalAmount);
         return new ResponseEntity(withdrawal, HttpStatus.OK);
     }
     @Operation(summary = "find a  Withdrawal by id", description = "initiate withdrawal process with the given information of the investor.")
     @GetMapping("/{id}")
     public ResponseEntity findById(@PathVariable Integer id){
         return new ResponseEntity<>(service.findById(id),HttpStatus.OK);
+    }
+    @GetMapping("investor/{investorId}/product/{productId}")
+    public ResponseEntity findByInvestorIdAndProductId(@PathVariable("investorId") String investorId, @PathVariable("productId") String productId){
+        return new ResponseEntity<>(service.findByInvestorIdAndProductId(investorId, productId),HttpStatus.OK);
     }
 }
