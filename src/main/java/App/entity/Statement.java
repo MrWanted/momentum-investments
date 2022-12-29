4821e5267f8c81f2a079c2198ad72fd9ec1a57b8
MrWanted <wanted.lepota@gmail.com>
2022-12-29 07:32:54 +0200

    update column names

--- src/main/java/App/entity/Statement.java
+++ src/main/java/App/entity/Statement.java
@@ -7,17 +7,20 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Entity
-@NoArgsConstructor
-@Setter
-@Getter
-public class Withdrawal extends Auditable<String> implements Serializable {
+@Data
+public class Statement extends Auditable<String> implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     private Long id;
+    @Column(name = "investor_id")
     private String investorId;
+
+    @Column(name = "product_id")
     private String productId;
-    @Column(name = "withdrawal-amount")
+    @Column(name = "available_balance")
+    private BigDecimal balance;
+    @Column(name = "withdrawal_amount")
     private BigDecimal withdrawalAmount;
     private String status;
 }
