2ea4c572fb84eab35c35b52fe33e71a0ccf92c46
MrWanted <wanted.lepota@gmail.com>
2023-01-05 20:59:58 +0200

    add SecurityConfig

--- src/main/java/App/config/SecurityConfig.java
+++ src/main/java/App/config/SecurityConfig.java
@@ -2,8 +2,12 @@ package App.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -11,20 +15,24 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public InMemoryUserDetailsManager user(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin")
+                        .password("{noop}password")
+                        .authorities("read")
+                        .build());
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         //Since H2 console runs within a frame so while Spring security is enabled, frame options has to be disabled explicitly, in order to get the H2 console working.
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+        httpSecurity.headers().frameOptions().disable();
 
-        return http
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/").authenticated();
-                    auth.anyRequest().permitAll();
-                })
-                .httpBasic(withDefaults())
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
                 .build();
     }
-
 }
