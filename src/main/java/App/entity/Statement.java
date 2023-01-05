cbf4fe0482dd2b2dbcec7440f33966ccdf8ffcff
MrWanted <wanted.lepota@gmail.com>
2023-01-05 11:40:38 +0200

    add sequence

--- src/main/java/App/entity/Statement.java
+++ src/main/java/App/entity/Statement.java
@@ -10,13 +10,13 @@ import java.math.BigDecimal;
 
 @Entity
 @Data
+@SequenceGenerator(name = "sequence", sequenceName = "statement-sequence")
 public class Statement extends Auditable<String> implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence")
     private Integer id;
     @Column(name = "investor_id")
     private String investorId;
-
     @Column(name = "product_id")
     private String productId;
     @Column(name = "available_balance")
