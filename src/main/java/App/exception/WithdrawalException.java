4475cb31f751f18278b25e500d3cba838832d745
MrWanted <wanted.lepota@gmail.com>
2023-01-06 22:42:46 +0200

    add error message

--- src/main/java/App/exception/WithdrawalException.java
+++ src/main/java/App/exception/WithdrawalException.java
@@ -3,6 +3,8 @@ package App.exception;
 import App.Error;
 
 public class WithdrawalException extends RuntimeException{
-    public WithdrawalException(Error error) {
+    private String message;
+    public WithdrawalException(String message) {
+        this.message = message;
     }
 }
