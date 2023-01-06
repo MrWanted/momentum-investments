ab2fe259b121e5326771260f6ac0c1c71b575e6f
MrWanted <wanted.lepota@gmail.com>
2023-01-06 22:47:59 +0200

    remove redundant method

--- src/main/java/App/service/StatementService.java
+++ src/main/java/App/service/StatementService.java
@@ -8,11 +8,8 @@ import java.util.List;
 
 public interface StatementService {
     Statement save(Statement withdrawal);
-
     Statement findByInvestorIdAndProductId(String investorId, String productId);
-    Statement updateWithdrawal(Statement withdrawal, Integer withdrawalId);
     List<Statement> findAll();
     Statement submitWithdrawal(String investorId, String productId, BigDecimal withdrawalAmount);
     Statement findById(Integer id);
-    Statement update(WithdrawVO withdrawal);
 }
