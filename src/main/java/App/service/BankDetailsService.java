e082e18a6c5bd6305ea124b95557912fe827078c
MrWanted <wanted.lepota@gmail.com>
2023-01-08 11:43:35 +0200

    organize imports

--- src/main/java/App/service/BankDetailsService.java
+++ src/main/java/App/service/BankDetailsService.java
@@ -2,7 +2,6 @@ package App.service;
 
 import App.entity.BankDetails;
 import java.util.List;
-import java.util.Optional;
 
 public interface BankDetailsService {
     List<BankDetails> findAll();
--- src/main/java/App/serviceImpl/BankDetailsServiceImpl.java
+++ src/main/java/App/serviceImpl/BankDetailsServiceImpl.java
@@ -7,9 +7,7 @@ import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
 import java.util.List;
-import java.util.Optional;
 
 @Data
 @Service
